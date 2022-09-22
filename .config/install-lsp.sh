2a6513aeb79e100f2d4439209958d853cdbf9152
Shawn Allen Converse <68838748+shawnaconverse@users.noreply.github.com>
2022-09-22 08:45:15 -0700

    update of configs

---
+++ .config/install-lsp.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# Lua
+brew install lua-language-server
+
+# Python
+# assumes python3/pip3 commands
+pip3 install pyright
+
+# Javascript and Typescript
+npm install -g typescript-language-server typescript
+
+# HTML and CSS
+npm install -g emmet-ls
+
+# Rust
+rustup component add rust-src
+
