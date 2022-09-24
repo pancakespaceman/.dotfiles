85c772b17fce9fb567dbef709b71f9676bf979c3
Shawn Allen Converse <68838748+shawnaconverse@users.noreply.github.com>
2022-09-23 23:50:53 -0700

    backup up personal vim config to use AstroNvim

---
+++ .config/nvimbackup/init.lua
@@ -0,0 +1,10 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.cmd [[packadd packer.nvim]]
+end
+
+require("core")
---
+++ .config/nvimbackup/lua/core/plugins.lua
@@ -0,0 +1,52 @@
+require('packer').startup(function(use)
+  -- Package Manager
+  use 'wbthomason/packer.nvim'
+
+  -- Git
+  use 'tpope/vim-fugitive'                                                  -- Git commands in nvim
+  use 'tpope/vim-rhubarb'                                                   -- Fugitive-companion to interact with github
+  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } } -- Add git related info in the signs columns and popups
+
+  -- Commenting
+  use 'numToStr/Comment.nvim'                                               -- "gc" to comment visual regions/lines
+
+  -- Syntax Highlighting
+  use 'nvim-treesitter/nvim-treesitter'                                     -- Highlight, edit, and navigate code
+  use 'nvim-treesitter/nvim-treesitter-textobjects'                         -- Additional textobjects for treesitter
+
+  -- LSP
+  -- use 'williamboman/mason.nvim'                                             -- Manage external editor tooling i.e LSP servers
+  -- use 'williamboman/mason-lspconfig.nvim'                                   -- Automatically install language servers to stdpath
+  use 'neovim/nvim-lspconfig'                                               -- Collection of configurations for built-in LSP client
+
+  -- Auto Complete
+  use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } }         -- Autocompletion
+
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+
+  -- Snippet Engine
+  use { 'L3MON4D3/LuaSnip', requires = { 'saadparwaiz1/cmp_luasnip' } }     -- Snippet Engine and Snippet Expansion
+
+  -- Colorschemes
+  use 'mjlbach/onedark.nvim'                                                -- Theme inspired by Atom
+  use 'rebelot/kanagawa.nvim'
+
+  -- Status line
+  use 'nvim-lualine/lualine.nvim'                                           -- Fancier statusline
+
+  -- Indentation guides
+  use 'lukas-reineke/indent-blankline.nvim'                                 -- Add indentation guides even on blank lines
+
+  -- Tabstop
+  use 'tpope/vim-sleuth'                                                    -- Detect tabstop and shiftwidth automatically
+
+  -- Fuzzy Finder (files, lsp, etc)
+  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable "make" == 1 }
+
+end)
